9dd171e8785466b8a54f5b744c614a0e4161b4a0
CharlesZuo01 <charlesqzuo@gmail.com>
2025-03-06 21:13:55 -0800

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "server_username" {
-  description = "server username"
-  default     = "charleszuo"
-}
-
-variable "server_password" {
-  description = "server password"
-  default     = "mypassword123!"
-}
